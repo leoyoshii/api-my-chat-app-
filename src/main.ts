e6262711ee325778e3559dbfbd6b92352132ee36
Leonardo Yoshii <leonardo.yoshii@objective.com.br>
2022-09-01 23:24:48 -0300

    chore(deps): add and configure Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import {NestFactory} from '@nestjs/core'
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 import {AppModule} from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('My Chat Application - Swagger')
+    .setDescription('The')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
