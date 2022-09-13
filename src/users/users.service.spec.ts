91d6826c920dc75eb76de93be38cd12ea82ba20d
Leonardo Yoshii <leonardo.yoshii@objective.com.br>
2022-09-13 00:10:29 -0300

    test(users): add some tests in Service, increasing coverage

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,58 +1,145 @@
+import {faker} from '@faker-js/faker'
 import {Test, TestingModule} from '@nestjs/testing'
+import {Users} from '@prisma/client'
 import {PasswordService} from '../services/password/password.service'
 import {PrismaService} from '../services/prisma/prisma.service'
+import {CreateUserDto} from './dto/create-user.dto'
 
 import {UsersService} from './users.service'
 
+function createRandomUsers(lenght: number): Users[] {
+  const usersArray: Users[] = []
+
+  Array.from({length: lenght}).forEach(() => {
+    const dateCreated = faker.date.recent()
+
+    const userCreated: Users = {
+      id: faker.datatype.uuid(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      createdAt: dateCreated,
+      updatedAt: dateCreated,
+      deletedAt: null,
+    }
+    usersArray.push(userCreated)
+  })
+
+  return usersArray
+}
+
+const UsersDb = createRandomUsers(10)
+
+const oneUser = UsersDb[0]
+
+const db = {
+  users: {
+    findMany: jest.fn().mockResolvedValue(UsersDb),
+    findUnique: jest.fn().mockResolvedValue(oneUser),
+    create: jest.fn().mockImplementation(async args => {
+      return Promise.resolve({...oneUser, ...args.data})
+    }),
+    update: jest.fn().mockImplementation(async args => {
+      return Promise.resolve({...oneUser, ...args.data})
+    }),
+  },
+}
+
 describe('UsersService', () => {
   let service: UsersService
-  let prismaService: PrismaService
+  let passwordService: PasswordService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, PrismaService, PasswordService],
+      providers: [UsersService, {provide: PrismaService, useValue: db}, PasswordService],
     }).compile()
 
     service = module.get<UsersService>(UsersService)
-    prismaService = module.get<PrismaService>(PrismaService)
+    passwordService = module.get<PasswordService>(PasswordService)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
-  //TODO: Create a mock of prisma.
 
-  // it('should create a User', async () => {
-  //   const password = faker.internet.password()
+  it('should create a User', async () => {
+    const password = faker.internet.password()
 
-  //   const user: CreateUserDto = {
-  //     firstName: faker.name.firstName(),
-  //     lastName: faker.name.lastName(),
-  //     email: faker.internet.email(),
-  //     password: password,
-  //     confirmPassword: password,
-  //   }
+    const user: CreateUserDto = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: password,
+      confirmPassword: password,
+    }
 
-  //   const userCreated = await service.create(user)
+    jest.spyOn(passwordService, 'hashPassword').mockImplementation(() => Promise.resolve(password))
 
-  //   expect(userCreated).toBe(user)
-  // })
+    const userCreated = await service.create(user)
 
-  // it('should find All users', async () => {
-  //   const users = await service.findAll()
+    const {confirmPassword, ...userRest} = user
 
-  //   expect(users).toBe([])
-  // })
+    expect(userCreated).toStrictEqual({...oneUser, ...userRest})
+  })
 
-  // it.todo('should find a user', () => {
-  //   expect(service).toBeDefined()
-  // })
+  it('should try create a User but password and confirmPassword is not the same', async () => {
+    const password = faker.internet.password()
 
-  // it.todo('should update a user', () => {
-  //   expect(service).toBeDefined()
-  // })
+    const user: CreateUserDto = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: password,
+      confirmPassword: 'notPassword',
+    }
 
-  // it.todo('should soft delete a user', () => {
-  //   expect(service).toBeDefined()
-  // })
+    jest.spyOn(passwordService, 'hashPassword').mockImplementation(() => Promise.resolve(password))
+
+    try {
+      await service.create(user)
+      expect('here').not.toBe('here')
+    } catch (error) {
+      expect(error.message).toContain('password and confirmPassword is not the same')
+    }
+  })
+
+  it('should find All users', async () => {
+    const users = await service.findAll()
+
+    expect(users).toStrictEqual(UsersDb)
+  })
+
+  it('should find a user', async () => {
+    const user = await service.findOne(oneUser.id)
+    expect(user).toStrictEqual(oneUser)
+  })
+
+  it('should update a user', async () => {
+    const fakerUpdatedFistname = faker.name.firstName()
+    const fakerUpdatedLastName = faker.name.lastName()
+
+    const user = await service.update(oneUser.id, {
+      firstName: fakerUpdatedFistname,
+      lastName: fakerUpdatedLastName,
+    })
+
+    const uptadedUser: Users = {
+      ...oneUser,
+      firstName: fakerUpdatedFistname,
+      lastName: fakerUpdatedLastName,
+    }
+
+    expect(user).toStrictEqual(uptadedUser)
+  })
+
+  it('should soft delete a user', async () => {
+    const user = await service.remove(oneUser.id)
+
+    expect(user).toStrictEqual(oneUser)
+
+    const user2 = await service.remove(oneUser.id)
+
+    expect(user2).toStrictEqual(oneUser)
+  })
 })
